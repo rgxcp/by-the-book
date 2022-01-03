90b429821c681b4177f528e54dcc71861f7b5480
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-03 11:36:37 +0700

    day 3.1
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ golang-book/chapter7/fibonacci.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func main() {
+	fmt.Println(fibonacci(0))  // 0
+	fmt.Println(fibonacci(1))  // 1
+	fmt.Println(fibonacci(10)) // 55
+}
