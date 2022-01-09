bf815e03da7ca911a7225b03a9ecf4da0f15b4ae
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-09 16:02:43 +0700

    day 8
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ golang-book/chapter12/math_test.go
@@ -0,0 +1,42 @@
+package math
+
+import "testing"
+
+// test file naming guide
+// file: "foo.go"
+// test file: "foo_test.go"
+
+// test function naming guide
+// function name: "Average"
+// test function name: "TestAverage"
+
+type testpair struct {
+	values  []float64
+	average float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 0},
+}
+
+func TestAverage(t *testing.T) {
+	var v float64
+	v = Average([]float64{1, 2})
+	if v != 1.5 {
+		t.Error("Expected 1.5, got ", v)
+	}
+
+	// run in bulk
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}
